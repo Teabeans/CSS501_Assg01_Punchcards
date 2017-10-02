84e86d81740727dd34eb2b55145b952ee6b0db89
Tim Lum <twhlum@gmail.com>
2017-10-02 02:38:21 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,103 @@
+// Tim Lum
+// twhlum@gmail.com
+// 2017.10.011
+// For the University of Washington Bothell CSS 501A
+// Autumn 2017, Graduate Certificate in Software Design & Development (GCSDD)
+//
+// File Description:
+// <Short explanation of file purpose and function>
+//
+// (If this file contains 'main()' ) Program Description:
+// <Explanation of overall program purpose and function>
+//
+// Required files:
+// <Files needed to run this program>
+//
+// Acknowledgements:
+// Source material from:
+// University of Washington Bothell
+// CSS 501A Data Structures And Object-Oriented Programming I
+// "Design and Coding Standards"
+// Michael Stiber
+//
+// File author:
+// Tim Lum (twhlum@gmail.com)
+//
+// License:
+// This software is published under the GNU general license which guarantees
+// end users the freedom to run, study, share and modify the software.
+// https://www.gnu.org/licenses/gpl.html
+//
+// Code Standards:
+// I. Comment at beginning of file (above) stating (at a minimum):
+//    A. File Name
+//    B. Author Name
+//    C. Date
+//    D. Description of code purpose 
+// II. Indentation:
+//    A. 3 whitespaces ("   ")
+//    B. May vary depending on language and instructor
+// III. Variables:
+//    A. Descriptive, legible name
+//    B. Comment over any variable declaration describing:
+//       0. Its use in the algorithm
+//       1. Invariant information such as legal ranges of values
+// IV. Class Files:
+//    A. Separate *.cpp and *.h files should be used for each class.
+//    B. Files names must exactly match class names (case-sensitive)
+// V. Includes:
+//    A. Calls for content ("#include") from the Standard Template Library (STL) should be formatted as follows:
+//       0. DO type:     #include <vector>
+//       1. Do NOT type: #include <vector.h>
+//    B. You may use the directive "using namespace std;"
+//       0. ??? (?CONFIRM?)
+// VI. Classes:
+//    A. Return values:
+//       0. Do NOT return references to internal class structures.
+//       1. Do NOT return pointers to internal class structures.
+//    B. Do NOT expose any details of the internal implementation.
+// VII. Functions + Methods:
+//    A. Functions should be used for appropriate operations.
+//    B. Reference arguments should be used only when necessary.
+//    C. The (return?CONFIRM?) type of each function must be declared
+//       0. Use 'void' when necessary
+//    D. Declare as 'const' (unalterable) when no modification is made to the object state
+//       0. UML 'query' property (?CONFIRM?)
+// VIII. Function Comments:
+//    A. DO include a comment prior to each function which includes the function's:
+//       0. Purpose - Why does the function exist?
+//       1. Parameters - What fields does the function contain?
+//       2. Preconditions - What conditions must be true prior to the function call?
+//       3. Postconditions - What conditions must be true after the function call?
+//       4. Return value - What is the nature and range of the value returned by the function?
+//       5. Functions called - What other functions are called by this function?
+// IX. Loop invariants
+//    A. Each loop should be commented with 'invariant' information (?CONFIRM?)
+// X. Assertions:
+//    A. May be comments or the 'assert()' feature.
+//    B. Insert where useful to explain important features or subtle logic.
+//    C. What, exactly, is an assertion (?CONFIRM?)
+// XI. Prohibited (unless justified):
+//    A. Global variables
+//    B. "Gotos" (?CONFIRM?)
+//
+// ---- BEGIN STUDENT CODE ----
+//
+// < HELD AS PRIVATE UNTIL GRADING COMPLETE >
+//
+// ---- END STUDENT CODE ----
+//
+#import <ThingsToImport>
+
+using namespace std;
+
+int main()<
+    cout << "Hello, World!" << endl;
+
+// <Variable Type> <Data Type> <Variable Name> <Assignment Operator> <Value>
+    const double PI = 3.14159;
+// "const" - Variable cannot be changed (similar to "final" in Java)
+// Convention is to capitalize 'const' variables (also similar to Java)
+
+    return 0;
+}
